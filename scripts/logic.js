59ecb1a5485462bbd1adccddf7e2bbf8fec93936
Sergey Papushin <spapushin@gmail.com>
2018-07-12 19:21:39 -0500

    Add logic to load macros file

---
+++ scripts/logic.js
@@ -0,0 +1,13 @@
+function loadMacros() {
+    $.ajax(
+        {
+            url: '/rando_files/macros.txt',
+            success: function (data) {
+                var doc = jsyaml.load(text);
+                console.log(doc);
+            }
+        }
+    )
+}
+
+loadMacros();
