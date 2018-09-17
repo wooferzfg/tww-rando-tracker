df2cd4953f80bc27f010513292a437fe1b45b08b
Sergey Papushin <spapushin@gmail.com>
2018-09-16 19:08:12 -0500

    Add support for commit numbers in version

--- scripts/launcher.js
+++ scripts/launcher.js
@@ -34,7 +34,7 @@ function applyflags(element) {
 			decodedData = decodedData.substring(1);
 		}
 
-		if (/^[0-9.]+$/.test(version) && version != currentVersion) {
+		if (/^[0-9a-f._]+$/.test(version) && !version.includes(currentVersion)) {
 			showBrokenPermalink(element, true);
 			return;
 		}
