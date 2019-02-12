5179cf0d141805d76556513d33f888fe7ca14c30
wooferzfg <spapushin@gmail.com>
2019-02-11 20:15:16 -0600

    Move entrances logic to separate file

--- scripts/main_logic.js
+++ scripts/main_logic.js
@@ -147,35 +147,6 @@ const shortDungeonNames = [
     'WT',
     'GT'
 ];
-const dungeonEntrances = [
-    'On Dragon Roost Island',
-    'In Forest Haven Sector',
-    'In Tower of the Gods Sector',
-    'On Headstone Island',
-    'On Gale Isle'
-];
-const caveEntrances = [
-    'Outset Island',
-    'Dragon Roost Island',
-    'Fire Mountain',
-    'Ice Ring Isle',
-    'Private Oasis',
-    'Needle Rock Isle',
-    'Angular Isles',
-    'Boating Course',
-    'Stone Watcher Island',
-    'Overlook Island',
-    "Bird's Peak Rock",
-    'Pawprint Isle',
-    'Pawprint Isle Side Isle',
-    'Diamond Steppe Island',
-    'Bomb Island',
-    'Rock Spire Isle',
-    'Shark Island',
-    'Cliff Plateau Isles',
-    'Horseshoe Island',
-    'Star Island'
-];
 
 // tracker should modify these
 var items = {
@@ -433,18 +404,4 @@ function incrementShield() {
         items["Hero's Shield"] = 0;
         items['Mirror Shield'] = 0;
     }
-}
-
-function getDungeonEntryName(index) {
-    return 'Entered ' + shortDungeonNames[index];
-}
-
-function getCaveName(index) {
-    return caves[index]
-        .replace('Secret ', '')
-        .replace('Warp Maze ', '');
-}
-
-function getCaveEntryName(index) {
-    return 'Entered ' + getCaveName(index);
 }
