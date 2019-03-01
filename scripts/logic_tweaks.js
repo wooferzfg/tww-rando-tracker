7a090ccadd6c48bdec8e8247e8ba5555ade13585
wooferzfg <spapushin@gmail.com>
2019-03-01 02:04:57 -0600

    No longer clear reefs when clearing all of FF in race mode

--- scripts/logic_tweaks.js
+++ scripts/logic_tweaks.js
@@ -86,12 +86,6 @@ function clearRaceModeBannedLocations() {
     } else if (currentGeneralLocation == "Forsaken Fortress") {
         mailbox["Letter from Aryll"] = true;
         mailbox["Letter from Tingle"] = true;
-        Object.keys(itemLocations).forEach(function (locationName) {
-            if (itemLocations[locationName].Types.includes("Eye Reef Chest")) {
-                var splitName = getSplitLocationName(locationName);
-                locationsChecked[splitName.general][splitName.detailed] = true;
-            }
-        });
     } else if (currentGeneralLocation == "Earth Temple") {
         mailbox["Letter from Baito"] = true;
     }
