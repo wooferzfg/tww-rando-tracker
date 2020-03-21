c228cc36b539ba8d649d2ebd36022f9e6910be45
wooferzfg <spapushin@gmail.com>
2020-03-20 22:38:59 -0500

    Set up initial shell for React development

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>YEET</div>,
+  document.getElementById('base')
+);
---
+++ src/logic/logic-loader.js
@@ -0,0 +1,3 @@
+export default class LogicLoader {
+  static loadLogic() {}
+}
