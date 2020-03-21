6f33e97860f85ce7cafa27a2d3bcd4f7ff359209
wooferzfg <spapushin@gmail.com>
2020-03-21 02:54:22 -0500

    Convert allLocations to mapLocations

--- src/services/logic/locations.test.js
+++ src/services/logic/locations.test.js
@@ -1,31 +1,35 @@
 import Locations from './locations';
 
-const ITEM_LOCATIONS_FILE = {
-  'Outset Island - Savage Labyrinth - Floor 30': {
-    Need: 'Can Access Savage Labyrinth & Can Defeat Keese & Can Defeat Miniblins',
-    'Original item': 'Triforce Chart 6',
-    Types: 'Savage Labyrinth',
-    Paths: ['Cave09/Room20.arc/Chest000']
-  },
-  "Dragon Roost Cavern - Bird's Nest": {
-    Need: 'Can Access Dragon Roost Cavern & DRC Small Key x3',
-    'Original item': 'Small Key',
-    Types: 'Dungeon',
-    Paths: ['M_NewD2/Room3.arc/Actor016']
-  }
-};
-
 describe('Locations', () => {
   describe('initialize', () => {
+    let itemLocationsFile;
+
+    beforeEach(() => {
+      itemLocationsFile = {
+        'Outset Island - Savage Labyrinth - Floor 30': {
+          Need: 'Can Access Savage Labyrinth & Can Defeat Keese & Can Defeat Miniblins',
+          'Original item': 'Triforce Chart 6',
+          Types: 'Savage Labyrinth',
+          Paths: ['Cave09/Room20.arc/Chest000']
+        },
+        "Dragon Roost Cavern - Bird's Nest": {
+          Need: 'Can Access Dragon Roost Cavern & DRC Small Key x3',
+          'Original item': 'Small Key',
+          Types: 'Dungeon',
+          Paths: ['M_NewD2/Room3.arc/Actor016']
+        }
+      };
+    });
+
     test('initializes the locations', () => {
-      Locations.initialize(ITEM_LOCATIONS_FILE);
+      Locations.initialize(itemLocationsFile);
 
       expect(Locations.locations).toMatchSnapshot();
     });
   });
 
-  describe('allLocations', () => {
-    test('creates an array with all location names', () => {
+  describe('mapLocations', () => {
+    beforeEach(() => {
       Locations.locations = {
         'Outset Island': {
           'Savage Labyrinth - Floor 30': {
@@ -38,8 +42,14 @@ describe('Locations', () => {
           }
         }
       };
+    });
 
-      expect(Locations.allLocations()).toMatchSnapshot();
+    test('creates an object by calling the iteratee on each location', () => {
+      const mappedLocations = Locations.mapLocations(
+        (generalLocation, detailedLocation) => `${detailedLocation} $$$ ${generalLocation}`
+      );
+
+      expect(mappedLocations).toMatchSnapshot();
     });
   });
 
