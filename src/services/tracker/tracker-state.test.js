f6377b290aad28fa8d41143491ae9a142582866a
wooferzfg <spapushin@gmail.com>
2020-03-21 16:23:22 -0500

    Clean up structure to make TrackerState not static

--- src/services/tracker/tracker-state.test.js
+++ src/services/tracker/tracker-state.test.js
@@ -1,8 +1,10 @@
+import _ from 'lodash';
+
 import TrackerState from './tracker-state';
 import Locations from '../logic/locations';
 
 describe('TrackerState', () => {
-  describe('initialize', () => {
+  describe('default', () => {
     beforeEach(() => {
       Locations.locations = {
         'Outset Island': {
@@ -19,21 +21,197 @@ describe('TrackerState', () => {
     });
 
     test('initializes the entrances as an empty object', () => {
-      TrackerState.initialize();
+      const defaultState = TrackerState.default();
 
-      expect(TrackerState.entrances).toEqual({});
+      expect(defaultState.entrances).toEqual({});
     });
 
     test('initializes the items', () => {
-      TrackerState.initialize();
+      const defaultState = TrackerState.default();
 
-      expect(TrackerState.items).toMatchSnapshot();
+      expect(defaultState.items).toMatchSnapshot();
     });
 
     test('initializes the checked locations', () => {
-      TrackerState.initialize();
+      const defaultState = TrackerState.default();
 
-      expect(TrackerState.locationsChecked).toMatchSnapshot();
+      expect(defaultState.locationsChecked).toMatchSnapshot();
+    });
+  });
+
+  describe('getItemValue', () => {
+    let state;
+
+    beforeEach(() => {
+      state = new TrackerState();
+      state.items = {
+        'Test Item': 2
+      };
+    });
+
+    test('returns the value of the item', () => {
+      const itemValue = state.getItemValue('Test Item');
+
+      expect(itemValue).toEqual(2);
+    });
+  });
+
+  describe('setItemValue', () => {
+    let state; let initialItems; let initialEntrances; let
+      initialLocationsChecked;
+
+    beforeEach(() => {
+      initialItems = {
+        'Test Item': 2
+      };
+      initialEntrances = {
+        'Test Entrance': 'Other Entrance'
+      };
+      initialLocationsChecked = {
+        'Dragon Roost Cavern': {
+          "Bird's Nest": true
+        }
+      };
+
+      state = new TrackerState();
+
+      state.items = _.clone(initialItems);
+      state.entrances = _.clone(initialEntrances);
+      state.locationsChecked = _.cloneDeep(initialLocationsChecked);
+    });
+
+    test('returns a new state with the item value modified', () => {
+      const newState = state.setItemValue('Test Item', 3);
+
+      expect(newState.items['Test Item']).toEqual(3);
+    });
+
+    test('keeps the other values unmodified', () => {
+      const newState = state.setItemValue('Test Item', 3);
+
+      expect(state.items).toEqual(initialItems);
+      expect(state.entrances).toEqual(initialEntrances);
+      expect(state.locationsChecked).toEqual(initialLocationsChecked);
+      expect(newState.entrances).toEqual(initialEntrances);
+      expect(newState.locationsChecked).toEqual(initialLocationsChecked);
+    });
+  });
+
+  describe('getEntranceValue', () => {
+    let state;
+
+    beforeEach(() => {
+      state = new TrackerState();
+      state.entrances = {
+        'Test Entrance': 'Other Entrance'
+      };
+    });
+
+    test('returns the value of the entrance', () => {
+      const entranceValue = state.getEntranceValue('Test Entrance');
+
+      expect(entranceValue).toEqual('Other Entrance');
+    });
+  });
+
+  describe('setEntranceValue', () => {
+    let state; let initialItems; let initialEntrances; let
+      initialLocationsChecked;
+
+    beforeEach(() => {
+      initialItems = {
+        'Test Item': 2
+      };
+      initialEntrances = {
+        'Test Entrance': 'Other Entrance'
+      };
+      initialLocationsChecked = {
+        'Dragon Roost Cavern': {
+          "Bird's Nest": true
+        }
+      };
+
+      state = new TrackerState();
+
+      state.items = _.clone(initialItems);
+      state.entrances = _.clone(initialEntrances);
+      state.locationsChecked = _.cloneDeep(initialLocationsChecked);
+    });
+
+    test('returns a new state with the entrance value modified', () => {
+      const newState = state.setEntranceValue('Test Entrance', 'Other Entrance 2');
+
+      expect(newState.entrances['Test Entrance']).toEqual('Other Entrance 2');
+    });
+
+    test('keeps the other values unmodified', () => {
+      const newState = state.setEntranceValue('Test Entrance', 'Other Entrance 2');
+
+      expect(state.items).toEqual(initialItems);
+      expect(state.entrances).toEqual(initialEntrances);
+      expect(state.locationsChecked).toEqual(initialLocationsChecked);
+      expect(newState.items).toEqual(initialItems);
+      expect(newState.locationsChecked).toEqual(initialLocationsChecked);
+    });
+  });
+
+  describe('isLocationChecked', () => {
+    let state;
+
+    beforeEach(() => {
+      state = new TrackerState();
+      state.locationsChecked = {
+        'Dragon Roost Cavern': {
+          "Bird's Nest": true
+        }
+      };
+    });
+
+    test('returns whether the location is checked', () => {
+      const isLocationChecked = state.isLocationChecked('Dragon Roost Cavern', "Bird's Nest");
+
+      expect(isLocationChecked).toEqual(true);
+    });
+  });
+
+  describe('setLocationChecked', () => {
+    let state; let initialItems; let initialEntrances; let
+      initialLocationsChecked;
+
+    beforeEach(() => {
+      initialItems = {
+        'Test Item': 2
+      };
+      initialEntrances = {
+        'Test Entrance': 'Other Entrance'
+      };
+      initialLocationsChecked = {
+        'Dragon Roost Cavern': {
+          "Bird's Nest": true
+        }
+      };
+
+      state = new TrackerState();
+
+      state.items = _.clone(initialItems);
+      state.entrances = _.clone(initialEntrances);
+      state.locationsChecked = _.cloneDeep(initialLocationsChecked);
+    });
+
+    test('returns a new state with the location checked value modified', () => {
+      const newState = state.setLocationChecked('Dragon Roost Cavern', "Bird's Nest", false);
+
+      expect(newState.locationsChecked['Dragon Roost Cavern']["Bird's Nest"]).toEqual(false);
+    });
+
+    test('keeps the other values unmodified', () => {
+      const newState = state.setLocationChecked('Dragon Roost Cavern', "Bird's Nest", false);
+
+      expect(state.items).toEqual(initialItems);
+      expect(state.entrances).toEqual(initialEntrances);
+      expect(state.locationsChecked).toEqual(initialLocationsChecked);
+      expect(newState.items).toEqual(initialItems);
+      expect(newState.entrances).toEqual(initialEntrances);
     });
   });
 });
