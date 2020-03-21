ad052ffbb27001b3e1fc373201bdf50c59246269
wooferzfg <spapushin@gmail.com>
2020-03-21 02:23:22 -0500

    Add macros

---
+++ src/services/logic/macros.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+
+export default class Macros {
+  static initialize(macrosFile) {
+    this.macros = macrosFile;
+  }
+
+  static getMacro(macroName) {
+    return _.get(this.macros, macroName);
+  }
+
+  static setMacro(macroName, value) {
+    _.set(this.macros, macroName, value);
+  }
+}
