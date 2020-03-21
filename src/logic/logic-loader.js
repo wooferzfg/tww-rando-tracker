e0b61b91dead00dc05b1bf3e553ad48a0465930a
wooferzfg <spapushin@gmail.com>
2020-03-20 22:58:51 -0500

    Add logic loader

--- src/logic/logic-loader.js
+++ src/logic/logic-loader.js
@@ -1,3 +1,28 @@
+import yaml from 'js-yaml';
+
 export default class LogicLoader {
-  static loadLogic() {}
+  static get VERSION() {
+    return '1.7.0';
+  }
+
+  static get LOGIC_FILES_URL_PREFIX() {
+    return `https://raw.githubusercontent.com/LagoLunatic/wwrando/${this.VERSION}/logic/`;
+  }
+
+  static async loadLogicFile(fileName) {
+    const response = await fetch(`${this.LOGIC_FILES_URL_PREFIX}/${fileName}`);
+    const fileData = await response.text();
+    const parsedFile = yaml.safeLoad(fileData);
+    return parsedFile;
+  }
+
+  static async loadLogicFiles() {
+    const itemLocations = await this.loadLogicFile('item_locations.txt');
+    const macros = await this.loadLogicFile('macros.txt');
+
+    return {
+      itemLocations,
+      macros
+    };
+  }
 }
