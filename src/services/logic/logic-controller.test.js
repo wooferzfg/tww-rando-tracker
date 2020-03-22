ef21cc4020a46c1ac6e5e0dce9a679a47f76f92f
wooferzfg <spapushin@gmail.com>
2020-03-22 14:07:28 -0500

    Add macros to logic tweaks

--- src/services/logic/logic-controller.test.js
+++ src/services/logic/logic-controller.test.js
@@ -1,4 +1,5 @@
 import LogicController from './logic-controller';
+import Settings from '../tracker/settings';
 
 describe('LogicController', () => {
   describe('allItems', () => {
@@ -6,6 +7,40 @@ describe('LogicController', () => {
       const allItems = LogicController.allItems();
 
       expect(allItems).toMatchSnapshot();
+    });
+  });
+
+  describe('isMainDungeon', () => {
+    describe('when the dungeon is a main dungeon', () => {
+      test('returns true', () => {
+        const isMainDungeon = LogicController.isMainDungeon('Dragon Roost Cavern');
+
+        expect(isMainDungeon).toEqual(true);
+      });
+    });
+
+    describe('when the dungeon is Forsaken Fortress', () => {
+      test('returns false', () => {
+        const isMainDungeon = LogicController.isMainDungeon('Forsaken Fortress');
+
+        expect(isMainDungeon).toEqual(false);
+      });
+    });
+
+    describe("when the dungeon is Ganon's Tower", () => {
+      test('returns false', () => {
+        const isMainDungeon = LogicController.isMainDungeon("Ganon's Tower");
+
+        expect(isMainDungeon).toEqual(false);
+      });
+    });
+
+    describe('when the argument is not a dungeon', () => {
+      test('returns false', () => {
+        const isMainDungeon = LogicController.isMainDungeon('Pawprint Isle');
+
+        expect(isMainDungeon).toEqual(false);
+      });
     });
   });
 
@@ -44,6 +79,108 @@ describe('LogicController', () => {
       const entryName = LogicController.caveEntryName('Dragon Roost Island Secret Cave');
 
       expect(entryName).toEqual('Entered Dragon Roost Island Cave');
+    });
+  });
+
+  describe('isRandomDungeonEntrances', () => {
+    describe('when dungeon entrances are randomized', () => {
+      beforeEach(() => {
+        Settings.initialize({
+          options: {
+            randomizeEntrances: 'Dungeons & Secret Caves (Separately)'
+          }
+        });
+      });
+
+      test('returns true', () => {
+        const isRandomDungeonEntrances = LogicController.isRandomDungeonEntrances();
+
+        expect(isRandomDungeonEntrances).toEqual(true);
+      });
+    });
+
+    describe('when dungeon entrances are not randomized', () => {
+      beforeEach(() => {
+        Settings.initialize({
+          options: {
+            randomizeEntrances: 'Secret Caves'
+          }
+        });
+      });
+
+      test('returns false', () => {
+        const isRandomDungeonEntrances = LogicController.isRandomDungeonEntrances();
+
+        expect(isRandomDungeonEntrances).toEqual(false);
+      });
+    });
+  });
+
+  describe('isRandomCaveEntrances', () => {
+    describe('when cave entrances are randomized', () => {
+      beforeEach(() => {
+        Settings.initialize({
+          options: {
+            randomizeEntrances: 'Dungeons & Secret Caves (Separately)'
+          }
+        });
+      });
+
+      test('returns true', () => {
+        const isRandomCaveEntrances = LogicController.isRandomCaveEntrances();
+
+        expect(isRandomCaveEntrances).toEqual(true);
+      });
+    });
+
+    describe('when cave entrances are not randomized', () => {
+      beforeEach(() => {
+        Settings.initialize({
+          options: {
+            randomizeEntrances: 'Dungeons'
+          }
+        });
+      });
+
+      test('returns false', () => {
+        const isRandomCaveEntrances = LogicController.isRandomCaveEntrances();
+
+        expect(isRandomCaveEntrances).toEqual(false);
+      });
+    });
+  });
+
+  describe('isRandomEntrancesTogether', () => {
+    describe('when dungeon and cave entrances are randomized together', () => {
+      beforeEach(() => {
+        Settings.initialize({
+          options: {
+            randomizeEntrances: 'Dungeons & Secret Caves (Together)'
+          }
+        });
+      });
+
+      test('returns true', () => {
+        const isRandomEntrancesTogether = LogicController.isRandomEntrancesTogether();
+
+        expect(isRandomEntrancesTogether).toEqual(true);
+      });
+    });
+
+    describe('when dungeon and cave entrances are not randomized together', () => {
+      beforeEach(() => {
+        Settings.initialize({
+          options: {
+            randomizeEntrances: 'Dungeons & Secret Caves (Separately)'
+          }
+        });
+      });
+
+      test('returns false', () => {
+        const isRandomEntrancesTogether = LogicController.isRandomEntrancesTogether();
+
+        expect(isRandomEntrancesTogether).toEqual(false);
+      });
     });
   });
 });
--- src/services/logic/logic-tweaks.js
+++ src/services/logic/logic-tweaks.js
@@ -1,12 +1,27 @@
 import _ from 'lodash';
 
+import CAVES from '../../data/caves';
+import CHARTS from '../../data/charts';
+import DUNGEONS from '../../data/dungeons';
+
 import Locations from './locations';
+import LogicController from './logic-controller';
+import Macros from './macros';
+import Settings from '../tracker/settings';
 
 export default class LogicTweaks {
   static updateLocations() {
     this._addDefeatGanondorf();
     this._updateTingleStatueReward();
     this._applyHasAccessedLocationTweaksForLocations();
+  }
+
+  static updateMacros() {
+    this._updateDungeonEntranceMacros();
+    this._updateCaveEntranceMacros();
+    this._updateChartMacros();
+    this._updateTriforceMacro();
+    this._applyHasAccessedLocationTweaksForMacros();
   }
 
   /**
@@ -67,5 +82,55 @@ export default class LogicTweaks {
         );
       });
     });
+  }
+
+  static _applyHasAccessedLocationTweaksForMacros() {
+    const macrosTweaks = this._HAS_ACCESSED_LOCATION_TWEAKS.macros;
+    _.forEach(macrosTweaks, (macroName) => {
+      const macroValue = Macros.getMacro(macroName);
+      const newMacro = this._replaceCanAccessOtherLocation(macroValue);
+      Macros.setMacro(macroName, newMacro);
+    });
+  }
+
+  static _canAccessMacroName(locationName) {
+    return `Can Access ${locationName}`;
+  }
+
+  static _updateDungeonEntranceMacros() {
+    if (LogicController.isRandomDungeonEntrances()) {
+      _.forEach(DUNGEONS, (dungeon) => {
+        if (LogicController.isMainDungeon(dungeon)) {
+          const macroName = this._canAccessMacroName(dungeon);
+          const entryName = LogicController.dungeonEntryName(dungeon);
+          Macros.setMacro(macroName, entryName);
+        }
+      });
+    }
+  }
+
+  static _updateCaveEntranceMacros() {
+    if (LogicController.isRandomCaveEntrances()) {
+      _.forEach(CAVES, (cave) => {
+        const macroName = this._canAccessMacroName(cave);
+        const entryName = LogicController.caveEntryName(cave);
+        Macros.setMacro(macroName, entryName);
+      });
+    }
+  }
+
+  static _updateChartMacros() {
+    if (Settings.getOptionValue('randomizeCharts')) {
+      _.forEach(CHARTS, (chart, index) => {
+        // We assume everything is a Treasure Chart and clear any additional requirements like
+        // wallet upgrades
+        const macroName = `Chart for Island ${index + 1}`;
+        Macros.setMacro(macroName, chart);
+      });
+    }
+  }
+
+  static _updateTriforceMacro() {
+    Macros.setMacro('All 8 Triforce Shards', 'Triforce Shard x8');
   }
 }
