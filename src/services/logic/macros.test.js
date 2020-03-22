0f55f9d7b9aaa8ede51789c71edc48177d9a59bc
wooferzfg <spapushin@gmail.com>
2020-03-21 23:39:34 -0500

    Add tests for macros

---
+++ src/services/logic/macros.test.js
@@ -0,0 +1,39 @@
+import Macros from './macros';
+
+describe('Macros', () => {
+  describe('initialize', () => {
+    test('initializes the macros', () => {
+      const macros = {
+        "Can Play Wind's Requiem": "Wind Waker & Wind's Requiem"
+      };
+
+      Macros.initialize(macros);
+
+      expect(Macros.macros).toEqual(macros);
+    });
+  });
+
+  describe('getMacro', () => {
+    test('returns the value of a macro', () => {
+      Macros.macros = {
+        "Can Play Wind's Requiem": "Wind Waker & Wind's Requiem"
+      };
+
+      const macroValue = Macros.getMacro("Can Play Wind's Requiem");
+
+      expect(macroValue).toEqual("Wind Waker & Wind's Requiem");
+    });
+  });
+
+  describe('setMacro', () => {
+    test('sets the value of a macro', () => {
+      Macros.macros = {
+        "Can Play Wind's Requiem": "Wind Waker & Wind's Requiem"
+      };
+
+      Macros.setMacro("Can Play Wind's Requiem", 'Mirror Shield');
+
+      expect(Macros.macros["Can Play Wind's Requiem"]).toEqual('Mirror Shield');
+    });
+  });
+});
