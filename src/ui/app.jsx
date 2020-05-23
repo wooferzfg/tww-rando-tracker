b5f4faf417407f1594d739a4a0925023c60449cf
wooferzfg <spapushin@gmail.com>
2020-05-23 18:11:17 -0500

    Fix UI exports

--- src/ui/app.jsx
+++ src/ui/app.jsx
@@ -10,17 +10,15 @@ import Tracker from './tracker';
 
 import '../css/main.scss';
 
-export default function App() {
-  return (
-    <HashRouter>
-      <Switch>
-        <Route exact path="/">
-          <Launcher />
-        </Route>
-        <Route exact path="/tracker">
-          <Tracker />
-        </Route>
-      </Switch>
-    </HashRouter>
-  );
-}
+export default () => (
+  <HashRouter>
+    <Switch>
+      <Route exact path="/">
+        <Launcher />
+      </Route>
+      <Route exact path="/tracker">
+        <Tracker />
+      </Route>
+    </Switch>
+  </HashRouter>
+);
