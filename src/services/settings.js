533802a756f4d4a452beea6ba5b428bc1559ea04
wooferzfg <spapushin@gmail.com>
2020-05-31 12:40:15 -0500

    Clean up flags mapping

--- src/services/settings.js
+++ src/services/settings.js
@@ -13,72 +13,12 @@ export default class Settings {
     this.startingGear = this.getOptionValue(Permalink.OPTIONS.STARTING_GEAR);
     this.version = this.getOptionValue(Permalink.OPTIONS.VERSION);
 
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_DUNGEONS)) {
-      this.flags.push(this.FLAGS.DUNGEON);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_GREAT_FAIRIES)) {
-      this.flags.push(this.FLAGS.GREAT_FAIRY);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_PUZZLE_SECRET_CAVES)) {
-      this.flags.push(this.FLAGS.PUZZLE_SECRET_CAVE);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_COMBAT_SECRET_CAVES)) {
-      this.flags.push(this.FLAGS.COMBAT_SECRET_CAVE);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_SHORT_SIDEQUESTS)) {
-      this.flags.push(this.FLAGS.SHORT_SIDEQUEST);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_LONG_SIDEQUESTS)) {
-      this.flags.push(this.FLAGS.LONG_SIDEQUEST);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_SPOILS_TRADING)) {
-      this.flags.push(this.FLAGS.SPOILS_TRADING);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_MINIGAMES)) {
-      this.flags.push(this.FLAGS.MINIGAME);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_FREE_GIFTS)) {
-      this.flags.push(this.FLAGS.FREE_GIFT);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_MAIL)) {
-      this.flags.push(this.FLAGS.MAIL);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_PLATFORMS_RAFTS)) {
-      this.flags.push(this.FLAGS.PLATFORM);
-      this.flags.push(this.FLAGS.RAFT);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_SUBMARINES)) {
-      this.flags.push(this.FLAGS.SUBMARINE);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_EYE_REEF_CHESTS)) {
-      this.flags.push(this.FLAGS.EYE_REEF_CHEST);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_BIG_OCTOS_GUNBOATS)) {
-      this.flags.push(this.FLAGS.BIG_OCTO);
-      this.flags.push(this.FLAGS.GUNBOAT);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_TREASURE_CHARTS)) {
-      this.flags.push(this.FLAGS.SUNKEN_TREASURE);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_EXPENSIVE_PURCHASES)) {
-      this.flags.push(this.FLAGS.EXPENSIVE_PURCHASE);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_MISC)) {
-      this.flags.push(this.FLAGS.OTHER_CHEST);
-      this.flags.push(this.FLAGS.MISC);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_TINGLE_CHESTS)) {
-      this.flags.push(this.FLAGS.TINGLE_CHEST);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_BATTLESQUID)) {
-      this.flags.push(this.FLAGS.BATTLESQUID);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_SAVAGE_LABYRINTH)) {
-      this.flags.push(this.FLAGS.SAVAGE_LABYRINTH);
-    }
-    if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_ISLAND_PUZZLES)) {
-      this.flags.push(this.FLAGS.ISLAND_PUZZLE);
-    }
+    _.forEach(this._FLAGS_MAPPING, (flagsForOption, optionName) => {
+      if (this.getOptionValue(optionName)) {
+        this.flags = _.concat(this.flags, flagsForOption);
+      }
+    });
+
     if (this.getOptionValue(Permalink.OPTIONS.PROGRESSION_TRIFORCE_CHARTS)) {
       if (this.getOptionValue(Permalink.OPTIONS.RANDOMIZE_CHARTS)) {
         this.flags.push(this.FLAGS.SUNKEN_TREASURE);
@@ -119,4 +59,37 @@ export default class Settings {
   static getVersion() {
     return this.version;
   }
+
+  static _FLAGS_MAPPING = {
+    [Permalink.OPTIONS.PROGRESSION_DUNGEONS]: [this.FLAGS.DUNGEON],
+    [Permalink.OPTIONS.PROGRESSION_GREAT_FAIRIES]: [this.FLAGS.GREAT_FAIRY],
+    [Permalink.OPTIONS.PROGRESSION_PUZZLE_SECRET_CAVES]: [this.FLAGS.PUZZLE_SECRET_CAVE],
+    [Permalink.OPTIONS.PROGRESSION_COMBAT_SECRET_CAVES]: [this.FLAGS.COMBAT_SECRET_CAVE],
+    [Permalink.OPTIONS.PROGRESSION_SHORT_SIDEQUESTS]: [this.FLAGS.SHORT_SIDEQUEST],
+    [Permalink.OPTIONS.PROGRESSION_LONG_SIDEQUESTS]: [this.FLAGS.LONG_SIDEQUEST],
+    [Permalink.OPTIONS.PROGRESSION_SPOILS_TRADING]: [this.FLAGS.SPOILS_TRADING],
+    [Permalink.OPTIONS.PROGRESSION_MINIGAMES]: [this.FLAGS.MINIGAME],
+    [Permalink.OPTIONS.PROGRESSION_FREE_GIFTS]: [this.FLAGS.FREE_GIFT],
+    [Permalink.OPTIONS.PROGRESSION_MAIL]: [this.FLAGS.MAIL],
+    [Permalink.OPTIONS.PROGRESSION_PLATFORMS_RAFTS]: [
+      this.FLAGS.PLATFORM,
+      this.FLAGS.RAFT
+    ],
+    [Permalink.OPTIONS.PROGRESSION_SUBMARINES]: [this.FLAGS.SUBMARINE],
+    [Permalink.OPTIONS.PROGRESSION_EYE_REEF_CHESTS]: [this.FLAGS.EYE_REEF_CHEST],
+    [Permalink.OPTIONS.PROGRESSION_BIG_OCTOS_GUNBOATS]: [
+      this.FLAGS.BIG_OCTO,
+      this.FLAGS.GUNBOAT
+    ],
+    [Permalink.OPTIONS.PROGRESSION_TREASURE_CHARTS]: [this.FLAGS.SUNKEN_TREASURE],
+    [Permalink.OPTIONS.PROGRESSION_EXPENSIVE_PURCHASES]: [this.FLAGS.EXPENSIVE_PURCHASE],
+    [Permalink.OPTIONS.PROGRESSION_MISC]: [
+      this.FLAGS.OTHER_CHEST,
+      this.FLAGS.MISC
+    ],
+    [Permalink.OPTIONS.PROGRESSION_TINGLE_CHESTS]: [this.FLAGS.TINGLE_CHEST],
+    [Permalink.OPTIONS.PROGRESSION_BATTLESQUID]: [this.FLAGS.BATTLESQUID],
+    [Permalink.OPTIONS.PROGRESSION_SAVAGE_LABYRINTH]: [this.FLAGS.SAVAGE_LABYRINTH],
+    [Permalink.OPTIONS.PROGRESSION_ISLAND_PUZZLES]: [this.FLAGS.ISLAND_PUZZLE]
+  };
 }
