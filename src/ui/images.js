23755ba4a55f9124a44b5bf114f64d85d3cc26a3
wooferzfg <spapushin@gmail.com>
2020-06-19 00:43:13 -0500

    Allow image imports to be nested

--- src/ui/images.js
+++ src/ui/images.js
@@ -1,27 +1,51 @@
 import _ from 'lodash';
 
+import LogicHelper from '../services/logic-helper';
+
 export default class Images {
   static get IMAGES() {
     return this.images;
   }
 
   static async importImages() {
-    const imageImports = {
-      SEA_CHART: import('../images/sea_chart.png'),
-    };
-
     this.images = {};
 
+    await this._resolveImports(this._IMAGE_IMPORTS);
+  }
+
+  static async _resolveImports(imageImports, keys = []) {
     await Promise.all(
       _.map(
         imageImports,
-        (imageImport, imageName) => this._loadImage(imageImport, imageName),
+        (importValue, importKey) => this._resolveImportValue(importValue, keys, importKey),
       ),
     );
   }
 
-  static async _loadImage(importPromise, imageName) {
+  static async _resolveImportValue(value, keys, newKey) {
+    const updatedKeys = _.concat(keys, newKey);
+
+    if (_.isPlainObject(value)) {
+      await this._resolveImports(value, updatedKeys);
+    } else {
+      await this._loadImage(value, updatedKeys);
+    }
+  }
+
+  static async _loadImage(importPromise, imageKeys) {
     const imageImport = await importPromise;
-    _.set(this.images, imageName, imageImport.default);
+    _.set(this.images, imageKeys, imageImport.default);
+  }
+
+  static get _IMAGE_IMPORTS() {
+    return {
+      SEA_CHART: import('../images/sea_chart.png'),
+      ITEMS: {
+        [LogicHelper.ITEMS.TELESCOPE]: {
+          0: import('../images/item0.png'),
+          1: import('../images/item0_a.png'),
+        },
+      },
+    };
   }
 }
