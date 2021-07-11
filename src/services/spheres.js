13ac4e6991acee066674aea148c3ada446903be8
wooferzfg <spapushin@gmail.com>
2021-07-11 18:13:43 -0500

    Add tests for Spheres

---
+++ src/services/spheres.js
@@ -0,0 +1,8 @@
+import Locations from './locations';
+import LogicCalculation from './logic-calculation';
+
+export default class Spheres {
+  static calculate(trackerState) {
+    return Locations.mapLocations(() => null);
+  }
+}
