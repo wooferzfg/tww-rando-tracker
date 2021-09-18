1c954a3f872202711ebf8e1b74ca128c1555582c
wooferzfg <spapushin@gmail.com>
2021-09-18 01:14:19 -0400

    Add documentation for private methods in BooleanExpression

--- src/services/boolean-expression.js
+++ src/services/boolean-expression.js
@@ -159,11 +159,38 @@ class BooleanExpression {
     return updatedExpression;
   }
 
+  /**
+   * The possible types for a boolean expression ('and'/'or').
+   *
+   * @property {object.<string, string>}
+   * @private
+   */
   static _TYPES = {
     AND: 'and',
     OR: 'or',
   };
 
+  /**
+   * Maps the boolean expression to a value. This is used to reduce the
+   * boolean expression to a boolean value (in `evaluate`) or to an arbitrary
+   * value (in `reduce`).
+   *
+   * @param {object} options The options for mapping.
+   * @param {Function} options.handleAnd A function that is called with two
+   *   arguments: the items in the expression, and a recursive mapping function.
+   *   The recursive mapping function takes an object as an argument with the
+   *   keys `item` (the current item) and `isMapped` (whether the item is a
+   *   nested boolean expression that has already been mapped). This function is
+   *   only called if the boolean expression has the 'and' type.
+   * @param {Function} options.handleOr A function that is called with two
+   *   arguments: the items in the expression, and a recursive mapping function.
+   *   The recursive mapping function takes an object as an argument with the
+   *   keys `item` (the current item) and `isMapped` (whether the item is a
+   *   nested boolean expression that has already been mapped). This function is
+   *   only called if the boolean expression has the 'or' type.
+   * @returns {any} The mapped value of the expression.
+   * @private
+   */
   _map({ handleAnd, handleOr }) {
     const recursiveMappingFunc = (item) => {
       if (BooleanExpression._isExpression(item)) {
@@ -192,6 +219,12 @@ class BooleanExpression {
     throw Error(`Invalid type: ${this.type}`);
   }
 
+  /**
+   * @returns {string} The opposite type of the current boolean expression. For
+   *   a boolean expression of type 'and', this method returns 'or', and vice
+   *   versa.
+   * @private
+   */
   _oppositeType() {
     if (this.isAnd()) {
       return BooleanExpression._TYPES.OR;
@@ -203,10 +236,29 @@ class BooleanExpression {
     throw Error(`Invalid type: ${this.type}`);
   }
 
+  /**
+   * @param {any} item The item or expression to check.
+   * @returns {boolean} Whether the given item is an instance of a boolean
+   *   expression.
+   * @private
+   */
   static _isExpression(item) {
     return item instanceof BooleanExpression;
   }
 
+  /**
+   * @param {object} options Options for evaluating if the expressions are
+   *   equal.
+   * @param {BooleanExpression} options.otherExpression The boolean expression
+   *   to compare the expression to.
+   * @param {Function} options.areItemsEqual A function that takes two arguments
+   *   that are items in the boolean expression. This function should return
+   *   whether the two items are equal.
+   * @returns {boolean} Whether the boolean expression is equal to the given
+   *   other boolean expression. The items in the two expressions can be in any
+   *   order, but they must otherwise be equivalent.
+   * @private
+   */
   _isEqualTo({
     otherExpression,
     areItemsEqual,
@@ -242,6 +294,12 @@ class BooleanExpression {
     return _.isEmpty(difference);
   }
 
+  /**
+   * @returns {BooleanExpression} The flattened boolean expression. For example,
+   *   the expression `And(And(And("X > 5")))` can be simplified to
+   *   `And("X > 5")`.
+   * @private
+   */
   _flatten() {
     const newItems = _.flatMap(this.items, (item) => {
       if (!BooleanExpression._isExpression(item)) {
@@ -275,11 +333,36 @@ class BooleanExpression {
     return new BooleanExpression(newItems, this.type);
   }
 
+  /**
+   * @param {any[]} items The items in the boolean expression. Each item can
+   *   either be an object of an arbitrary type or a boolean expression.
+   * @param {string} type The type of the boolean expression ('and'/'or');
+   * @returns {BooleanExpression} The new boolean expression with the given
+   *   items and type, modified to be flattened.
+   * @private
+   */
   static _createFlatExpression(items, type) {
     const newExpression = new BooleanExpression(items, type);
     return newExpression._flatten();
   }
 
+  /**
+   * @param {object} options Options for determining if the item is subsumed.
+   * @param {any[]} itemsCollection The collection of items to search through.
+   * @param {any} item The item to check.
+   * @param {string} expressionType The type of the expression.
+   * @param {Function} implies A function that takes two arguments that are both
+   *   items in the boolean expression. The function returns whether the first
+   *   item being true implies that the second item is true. For example,
+   *   "X > 5" would imply "X > 3".
+   * @returns {boolean} Whether the given item is subsumed by any item in the
+   *   given items collection.
+   *   For an 'and' expression, this is true if any of the items in the given
+   *   items collection imply the given item.
+   *   For an 'or' expression, this is true if the given item implies any of
+   *   the items in the given items collection.
+   * @private
+   */
   static _itemIsSubsumed({
     itemsCollection,
     item,
@@ -314,6 +397,16 @@ class BooleanExpression {
     return itemIsSubsumed;
   }
 
+  /**
+   * @param {object} parentItems An object with a key for each of the expression
+   *   types ('and'/'or'), with the values for each key being the items that
+   *   have appeared in parent expressions with that type.
+   * @returns {object} The updated parent items. All items in the current
+   *   boolean expression instance that are not boolean expressions are added
+   *   to the parent items object under the key corresponding to the current
+   *   type.
+   * @private
+   */
   _getUpdatedParentItems(parentItems) {
     return _.mergeWith(
       {},
@@ -331,6 +424,26 @@ class BooleanExpression {
     );
   }
 
+  /**
+   * @param {object} options The options for removing duplicate children.
+   * @param {Function} options.implies A function that takes two arguments that
+   *   are both items in the boolean expression. The function returns whether
+   *   the first item being true implies that the second item is true. For
+   *   example, "X > 5" would imply "X > 3".
+   * @param {object} options.parentItems An object with a key for each of the
+   *   expression types ('and'/'or'), with the values for each key being the
+   *   items that have appeared in parent expressions with that type.
+   * @returns {BooleanExpression} The current boolean expression instance, with
+   *   items removed if they are subsumed by any items that appear in parent
+   *   expressions of the current boolean expression instance.
+   *   If an expression has an item that also exists in a parent expression
+   *   with the same type, then that item is removed.
+   *   If an expression has an item that also exists in a parent expression
+   *   with the opposite type, then the entire expression is removed.
+   *   If all of an expression's items have been removed, then that expression's
+   *   parent is removed.
+   * @private
+   */
   _removeDuplicateChildrenHelper({
     implies,
     parentItems,
@@ -405,6 +518,16 @@ class BooleanExpression {
     };
   }
 
+  /**
+   * @param {Function} implies A function that takes two arguments that are both
+   *   items in the boolean expression. The function returns whether the first
+   *   item being true implies that the second item is true. For example,
+   *   "X > 5" would imply "X > 3".
+   * @returns {BooleanExpression} The current boolean expression instance, with
+   *   items removed if they are subsumed by any items that appear in parent
+   *   expressions of the current boolean expression instance.
+   * @private
+   */
   _removeDuplicateChildren(implies) {
     const { expression } = this._removeDuplicateChildrenHelper({
       implies,
@@ -417,6 +540,29 @@ class BooleanExpression {
     return expression;
   }
 
+  /**
+   * @param {object} options The options for determining whether the current
+   *   boolean expression is subsumed.
+   * @param {BooleanExpression} options.otherExpression The boolean expression
+   *   to compare the current boolean expression instance to.
+   * @param {Function} options.implies A function that takes two arguments that
+   *   are both items in the boolean expression. The function returns whether
+   *   the first item being true implies that the second item is true. For
+   *   example, "X > 5" would imply "X > 3".
+   * @param {boolean} options.removeIfIdentical Whether to remove the current
+   *   boolean expression instance if it is identical to the given other boolean
+   *   expression.
+   * @param {string} options.expressionType The type that the current boolean
+   *   expression instance should eventually have once it is flattened. This
+   *   will often be different from `this.type` since this function is called
+   *   recursively on a non-flattened boolean expression structure.
+   * @returns {boolean} Whether the current boolean expression instance is
+   *   subsumed by the given other boolean expression instance. This means that
+   *   in all cases, the other boolean expression instance implies the current
+   *   boolean expression instance, and therefore the current boolean expression
+   *   instance is redundant.
+   * @private
+   */
   _isSubsumedBy({
     otherExpression,
     implies,
@@ -452,6 +598,23 @@ class BooleanExpression {
     );
   }
 
+  /**
+   * @param {object} options The options for determining whether the given
+   *   expression is subsumed.
+   * @param {BooleanExpression} options.expressionToCheck The expression to
+   *   check.
+   * @param {number} options.index The index of the given boolean expression
+   *   within the items of the current boolean expression instance.
+   * @param {Function} options.implies A function that takes two arguments that
+   *   are both items in the boolean expression. The function returns whether
+   *   the first item being true implies that the second item is true. For
+   *   example, "X > 5" would imply "X > 3".
+   * @returns {boolean} Whether the given expression is subsumed by any other
+   *   item or expression in the items of the current boolean expression
+   *   instance. If multiple expressions are identical, then only the expression
+   *   with the lowest index is kept.
+   * @private
+   */
   _expressionIsSubsumed({ expressionToCheck, index, implies }) {
     let expressionIsSubsumed = false;
 
@@ -485,6 +648,15 @@ class BooleanExpression {
     return expressionIsSubsumed;
   }
 
+  /**
+   * @param {Function} implies A function that takes two arguments that are both
+   *   items in the boolean expression. The function returns whether the first
+   *   item being true implies that the second item is true. For example,
+   *   "X > 5" would imply "X > 3".
+   * @returns {BooleanExpression} The result of recursively calling
+   *   `_removeDuplicateExpressions` on any child boolean expressions.
+   * @private
+   */
   _removeDuplicateExpressionsInChildren(implies) {
     const newItems = _.map(this.items, (item) => {
       if (BooleanExpression._isExpression(item)) {
@@ -496,6 +668,15 @@ class BooleanExpression {
     return BooleanExpression._createFlatExpression(newItems, this.type);
   }
 
+  /**
+   * @param {Function} implies A function that takes two arguments that are both
+   *   items in the boolean expression. The function returns whether the first
+   *   item being true implies that the second item is true. For example,
+   *   "X > 5" would imply "X > 3".
+   * @returns {BooleanExpression} The current boolean expression instance, with
+   *   items removed if they are subsumed by any other items.
+   * @private
+   */
   _removeDuplicateExpressions(implies) {
     const parentExpression = this._removeDuplicateExpressionsInChildren(implies);
 
