654b23d085e5c53a4a30753709a468b722fe4ca5
wooferzfg <spapushin@gmail.com>
2022-12-13 11:08:55 -0500

    Update Tooltip to use useId

--- src/ui/tooltip.jsx
+++ src/ui/tooltip.jsx
@@ -1,40 +1,33 @@
 import _ from 'lodash';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useId } from 'react';
 import { Tooltip as ReactTooltip } from 'react-tooltip';
 import 'react-tooltip/dist/react-tooltip.css';
 
-class Tooltip extends React.PureComponent {
-  render() {
-    const {
-      children,
-      tooltipContent,
-    } = this.props;
-
-    if (_.isNil(tooltipContent)) {
-      return children;
-    }
-
-    const tooltipId = _.uniqueId('tooltip');
-
-    return (
-      <div
-        className="tooltip-wrapper"
-        data-tooltip-content
-        id={tooltipId}
-        data-tooltip-place="bottom"
-        data-tooltip-variant="light"
-      >
-        {children}
-        <ReactTooltip
-          anchorId={tooltipId}
-          className="tooltip-element"
-        >
-          {tooltipContent}
-        </ReactTooltip>
-      </div>
-    );
+function Tooltip({ children, tooltipContent }) {
+  if (_.isNil(tooltipContent)) {
+    return children;
   }
+
+  const tooltipId = useId();
+
+  return (
+    <div
+      className="tooltip-wrapper"
+      data-tooltip-content
+      id={tooltipId}
+      data-tooltip-place="bottom"
+      data-tooltip-variant="light"
+    >
+      {children}
+      <ReactTooltip
+        anchorId={tooltipId}
+        className="tooltip-element"
+      >
+        {tooltipContent}
+      </ReactTooltip>
+    </div>
+  );
 }
 
 Tooltip.defaultProps = {
