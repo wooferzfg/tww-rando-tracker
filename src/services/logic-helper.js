ff320628e8110ae90e750bab1c4642ab90eaa151
wooferzfg <spapushin@gmail.com>
2025-06-03 20:36:30 -0400

    Start with starting blue chu jelly count

--- src/services/logic-helper.js
+++ src/services/logic-helper.js
@@ -796,6 +796,7 @@ class LogicHelper {
         Permalink.OPTIONS.NUM_STARTING_TRIFORCE_SHARDS,
       ),
       [this.ITEMS.TINGLE_STATUE]: startingTingleStatues,
+      [this.BLUE_CHU_JELLY_COUNT_ITEM]: this.startingBlueChuJellyCount(),
     };
     this.impossibleItems = {};
 
